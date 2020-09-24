97d0271cf51dfc9bb5fd2873b2e8de4df0b5ecfe
Kim <fkqoseka@gmail.com>
2020-09-24 12:44:03 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <p>I'm youngseon</p>
+    </div>
+  );
+}
+
+export default App;
